97d88c98afa18d0e4a3edda1cb8f349d8cd09011
Tommaso Arone <153945012+aronetommaso@users.noreply.github.com>
2026-05-27 16:37:42 +0200

    Adding docker for graphdb

---
+++ graphDB.py
@@ -0,0 +1,26 @@
+import requests
+
+def upload_to_graphdb(file_path, repo_id):
+    print(f"Caricamento di {file_path} nel repository '{repo_id}' di GraphDB...")
+    
+    # Endpoint standard di GraphDB per l'inserimento dati via REST
+    url = f"http://localhost:7200/repositories/{repo_id}/statements"
+    
+    headers = {
+        "Content-Type": "text/turtle"
+    }
+    
+    try:
+        with open(file_path, 'rb') as f:
+            response = requests.put(url, data=f, headers=headers)
+            
+        if response.status_code in [200, 204]:
+            print("KG caricato con successo su GraphDB localhost!")
+        else:
+            print(f"Errore durante il caricamento: {response.status_code} - {response.text}")
+    except Exception as e:
+        print(f"Impossibile connettersi a GraphDB: {e}")
+
+if __name__ == '__main__':
+
+    upload_to_graphdb('acled_knowledge_graph.ttl', 'MiddleEastConflict')
